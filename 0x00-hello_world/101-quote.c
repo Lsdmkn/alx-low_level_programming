a985fda0f589c25ea8ff468f55d122a18fe216ae
Lsdmkn <lsdmkn@gmail.com>
2022-06-02 18:31:30 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,5 +6,7 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{ Write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",59);
-return (1) }
+{
+	Write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",59);
+	return (1);
+}
