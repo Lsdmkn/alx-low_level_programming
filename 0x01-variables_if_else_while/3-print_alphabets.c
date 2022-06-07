97a328a8fa3de24ff96e0406c7b02e61999fbd92
Lsdmkn <lsdmkn@gmail.com>
2022-06-06 20:43:05 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** main - Entry point of program
+ * prints lowercase and upper cases of the alphabet
+ * return: (0) Success
+ */
+int main(void)
+{
+
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; '\n' ; ch++)
+{
+putchar(ch);
+}
+
+return (0);
+}
