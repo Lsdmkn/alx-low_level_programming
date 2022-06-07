0bdb708403f6d5001d64b8496527a8a2d90dd3f7
Lsdmkn <lsdmkn@gmail.com>
2022-06-06 20:23:30 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,15 @@
  * Return: Always (0) Success
  */
 
-int main (void) {
-   char ch;
+int main(void)
+{
+char ch;
 
-   for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-      putchar('\n');
-   }
-   
-   return(0);
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+putchar('\n');
+}
+
+return (0);
 }
