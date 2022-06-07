c8633892a9888bb312e4381fd008872aafa521f6
Lsdmkn <lsdmkn@gmail.com>
2022-06-07 14:43:12 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
-#include <stdio>
+#include "main.h"
 
 /**
  * main - Entry point of program
- * prints text
- * Return: (0) Success
+ * prints putchar
+ * Return: always (0) Success
  */
 
 int main(void)
 {
-char ch = '_putchar';
-putchar(ch);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
-
