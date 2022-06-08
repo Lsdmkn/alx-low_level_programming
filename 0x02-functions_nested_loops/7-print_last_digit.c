1193e556b408df43d6160ac30d8b720fea3770db
Lsdmkn <lsdmkn@gmail.com>
2022-06-07 19:59:57 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,25 +2,17 @@
 
 /**
  * print_last_digit - prints the last digit
- * @n: passed args
+ * @n: the passed args
  * Return: the last digit
  */
 int print_last_digit(int n)
 {
 	int x;
-	int r;
 
 	if (n < 0)
-	{
 	n = -n;
 	x = n % 10;
 	_putchar(x + '0');
-	}
-	{
-	r = print_last_digit(INT_MIN);
-	_putchar('0' + r);
-	_putchar('\n');
-	
-	return (0);
-	}
+
+	return (x);
 }
