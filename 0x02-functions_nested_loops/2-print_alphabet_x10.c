3d1eee53d94823b5c00fff39d5443b1f01b7de2a
Lsdmkn <lsdmkn@gmail.com>
2022-06-07 17:34:55 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet ten times
+ */
+
+void print_alphabet_x10(void);
+{
+int ch;
+int x;
+
+for (x = 0; x < 10; x++)
+{
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+	_putchar(ch);
+}
+_putchae('\n');
+}
+}
+
