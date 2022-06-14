f36f6a7940635add30dfc720c99bf430982b6b7b
Lsdmkn <lsdmkn@gmail.com>
2022-06-13 19:54:37 -0700

    Finished Task 0 and main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the interger to 98
+ * @n: parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Answers to the 0x05. C-Pointers,arrays and strings projects
